407d7261b39059abc1655eb633cb8f0689acd2b7
Carl Emil Carlsen <34234131+cecarlsen@users.noreply.github.com>
2021-06-30 15:37:14 +0200

    0.1.2

--- Assets/TrackingTools/CHANGELOG.md
+++ Assets/TrackingTools/CHANGELOG.md
@@ -1,3 +1,9 @@
+##0.1.2 (2021/06/29)
+
+**CHANGES**
+
+ - Added load event options to ExtrinsicsLoader.
+
 ##0.1.1 (2021/06/03)
 
 **CHANGES**
--- Assets/TrackingTools/Runtime/ExtrinsicsLoader.cs
+++ Assets/TrackingTools/Runtime/ExtrinsicsLoader.cs
@@ -1,15 +1,17 @@
 ﻿/*
-	Copyright © Carl Emil Carlsen 2020
+	Copyright © Carl Emil Carlsen 2020-2021
 	http://cec.dk
 */
 
+using System;
 using UnityEngine;
 
 namespace TrackingTools
 {
 	public class ExtrinsicsLoader : MonoBehaviour
 	{
-		[SerializeField] string _extrinsicsFileName = "DefaultCamera";
+		[SerializeField] string _fileNameWithoutExtension = "DefaultCamera";
+		[SerializeField] LoadMode _mode = LoadMode.Awake;
 		[SerializeField,Tooltip("Optional")] Transform _anchorTransform = null;
 		[SerializeField] bool _inverse = false;
 		[SerializeField] bool _isMirrored = false;
@@ -19,23 +21,46 @@ namespace TrackingTools
 
 		Extrinsics _extrinsics;
 
+		[Serializable] enum LoadMode { None, Awake, Start }
+
+
+		public string fileNameWithoutExtension {
+			get { return _fileNameWithoutExtension; }
+			set {
+				_fileNameWithoutExtension = value;
+				Load();
+			}
+		}
+
 
 		void Awake()
 		{
-			if( !Extrinsics.TryLoadFromFile( _extrinsicsFileName, out _extrinsics ) ) {
-				enabled = false;
-				return;
-			}
+			if( _mode == LoadMode.Awake ) Load();
+		}
 
-			if( _anchorTransform && _imbedInAnchorTransform ) transform.SetParent( _anchorTransform );
 
-			_extrinsics.ApplyToTransform( transform, _anchorTransform, _inverse, _isMirrored, _space );
+		void Start()
+		{
+			if( _mode == LoadMode.Start ) Load();
 		}
 
 
 		void Update()
 		{
+			if( _extrinsics == null ) return;
+
 			if( _updateContinously ) _extrinsics.ApplyToTransform( transform, _anchorTransform, _inverse, _isMirrored, _space );
+		}
+
+
+
+		void Load()
+		{
+			if( !Extrinsics.TryLoadFromFile( _fileNameWithoutExtension, out _extrinsics ) ) return;
+
+			if( _anchorTransform && _imbedInAnchorTransform ) transform.SetParent( _anchorTransform );
+
+			_extrinsics.ApplyToTransform( transform, _anchorTransform, _inverse, _isMirrored, _space );
 		}
 	}
 }
